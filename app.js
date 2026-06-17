e77329a8998c458897354afca8539981c7ea65dc
MikaDev <camilo.ederson@gmail.com>
2026-06-17 14:02:30 -0600

    Disabled for my predictions

--- app.js
+++ app.js
@@ -333,6 +333,7 @@ async function init(){
   });
 
   document.getElementById('savePredBtn').addEventListener('click', async ()=>{
+    if(e.target.hasAttribute('disabled')) return;
     if(!currentPlayer){
       document.getElementById('predStatus').textContent = 'Primero elige o agrega tu nombre.'; return;
     }
